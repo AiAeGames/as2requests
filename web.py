c2f76adf9d41bd797b61fe4b5efd2afa1c3d7187
Daniel Dimitrov <bgdppeu@gmail.com>
2017-05-11 16:26:04 +0300

    New things

--- web.py
+++ web.py
@@ -60,6 +60,13 @@ def counter(id):
     results = q.fetchone()
     count["total"] = str(results["c"])
     arr.append(count)
+    # Status
+    count = {}
+    q = execute("SELECT bot, username FROM users WHERE id = %s", [id])
+    results = q.fetchone()
+    count["status"] = str(results["bot"])
+    count["username"] = str(results["username"])
+    arr.append(count)
     return arr
 
 @app.route('/')
@@ -67,12 +74,13 @@ def counter(id):
 def index(where='0'):
     api = request.cookies.get('api')
     list_requests = []
+    ca = ''
     if checker(api=api) == True:
         q = execute("SELECT * FROM users WHERE api=%s", [api])
         result = q.fetchone()
         info = result
         temp_request = {}
-        res = execute("SELECT * FROM requests WHERE user_id = %s and action = %s ORDER BY id and action = 0 asc", [info["id"], where])
+        res = execute("SELECT * FROM requests WHERE user_id = %s and action = %s ORDER BY id and action = 0 desc", [info["id"], where])
         results = res.fetchall()
         for row in results:
             temp_request["id"] = row["id"]
@@ -80,27 +88,39 @@ def index(where='0'):
             temp_request["requested_by"] = row["requested_by"]
             temp_request["song_id"] = row["song_id"]
             temp_request["title"] = row["title"]
-            temp_request["duration"] = row["duration"].split('PT')[1]
+            temp_request["duration"] = row["duration"].split('PT')[1].lower()
             temp_request["mode"] = row["mode"]
+            temp_request["status"] = row["status"]
             temp_request["action"] = row["action"]
             list_requests.append(temp_request)
             temp_request = {}
+        ca = counter(info["id"])
     else:
         info = None
         
-    return render_template('index.html', path=request.url_root, url=request.url, userinfo=info, list_requests=list_requests, where=where, stats=counter(info["id"]))
+    return render_template('index.html', path=request.url_root, url=request.url, userinfo=info, list_requests=list_requests, where=where, stats=ca)
 
+@app.route('/action/<action>/')
 @app.route('/action/<action>/<rid>')
-def action_delete(action, rid):
+def action_delete(action, rid = None):
     api = request.cookies.get('api')
     q = execute("SELECT * FROM users WHERE api=%s", [api])
     result = q.fetchone()
     if action == "done":
         execute("UPDATE requests SET action=1 WHERE user_id = %s and id = %s", [result["id"], rid])
+        execute("UPDATE requests SET status=0 WHERE user_id = %s and id = %s", [result["id"], rid])
     elif action == "ignore":
         execute("UPDATE requests SET action=2 WHERE user_id = %s and id = %s", [result["id"], rid])
+        execute("UPDATE requests SET status=0 WHERE user_id = %s and id = %s", [result["id"], rid])
     elif action == "delete":
         execute("DELETE FROM requests WHERE user_id = %s and id = %s", [result["id"], rid])
+    elif action == "playing":
+        execute("UPDATE requests SET status=1 WHERE user_id = %s and id = %s", [result["id"], rid])
+    elif action == "bot":
+        if result["bot"] == 1:
+            execute("UPDATE users SET bot=0 WHERE id = %s", [result["id"]])
+        else:
+            execute("UPDATE users SET bot=1 WHERE id = %s", [result["id"]])
     red = make_response(redirect('/'))
     return red
 
@@ -124,13 +144,24 @@ def twitch():
     red.set_cookie('api', key, expires=expire_date)
     return red
 
+@app.route('/playing/<username>/')
+def playing(username):
+    q = execute("SELECT id FROM users WHERE username=%s", [username])
+    result = q.fetchone()
+    q = execute("SELECT * FROM requests WHERE status=1 and user_id=%s", [result["id"]])
+    result = q.fetchone()
+    if result == None:
+        return render_template('playing.html', path=request.url_root, url=request.url, text="")
+    text = "Current song length {} requested by {}".format(result["duration"].split('PT')[1].lower(), result["requested_by"])
+    return render_template('playing.html', path=request.url_root, url=request.url, text=text)
+
 @app.errorhandler(404)
 def not_found(error):
     red = make_response(redirect('/'))
     return red
 
 if __name__ == "__main__":
-    #context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    #context.load_cert_chain('host.crt', 'aiae.key')
-    #ssl_context=context
-    app.run(debug=True, port=7294, threaded=False, host='0.0.0.0')
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain('host.crt', 'aiae.key')
+
+    app.run(debug=True, port=7294, ssl_context=context, threaded=False, host='0.0.0.0')
